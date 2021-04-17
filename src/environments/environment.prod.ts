4ff7f6623ce5c3c0b069c9c2fd2215a48f848375
SKGHD <sangramgorai@gmail.com>
2021-04-18 04:02:45 +0530

    Ready to use

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyB_t98ngk5O4vxNm6kRFtsNh_Vvr1YHx-0",
+    authDomain: "angular-http-4a6b3.firebaseapp.com",
+    databaseURL: "https://angular-http-4a6b3-default-rtdb.firebaseio.com",
+    projectId: "angular-http-4a6b3",
+    storageBucket: "angular-http-4a6b3.appspot.com",
+    messagingSenderId: "202400861809",
+    appId: "1:202400861809:web:c5a1a241e10afc178495d0",
+    measurementId: "G-F76C6J46BD"
+  }
 };
